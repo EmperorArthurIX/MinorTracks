2b38b207b604ed87d837569127b9ca28b4e57246
Ishaan Sunita Pandita <emperorarthurix@gmail.com>
2022-05-04 18:48:44 +0530

    Updated Modals, Added Navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 function modalshow(caller)
 {
-    let modal_id = caller+'_Modal';
+    let modal_id = 'Modal';
+    document.querySelector('#modal-text').innerText = `Data for Minor Track Number ${caller}`;
+    document.querySelector('#modal-institute').innerText = `Insitute that offers ${caller}`;
+    document.querySelector(".modal-title").innerText = `${caller} Name`;
     $("#"+modal_id).modal({backdrop: true});
 }
